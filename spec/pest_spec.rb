4832ac0c0814329a5b221f830f587a9ad5e72c8b
Michael Dippery <michael@monkey-robot.com>
2014-12-15 01:54:38 -0800

    Unit tests

---
+++ spec/pest_spec.rb
@@ -0,0 +1,31 @@
+require 'rack-pesticide'
+require 'rspec'
+
+
+describe "rack-pesticide" do
+  def app
+    Rack::Pesticide.new(nil)
+  end
+
+  describe "#pest?" do
+    it "should allow 'good' referers" do
+      env = {'HTTP_REFERER' => 'http://reddit.com/'}
+      app.send(:pest?, env).should be false
+    end
+
+    it "should block the semalt.com crawler" do
+      env = {'HTTP_REFERER' => 'http://semalt.com/crawler.php'}
+      app.send(:pest?, env).should be true
+    end
+
+    it "should block buttons-for-website.com" do
+      env = {'HTTP_REFERER' => 'http://buttons-for-website.com/'}
+      app.send(:pest?, env).should be true
+    end
+
+    it "should block makemoneyonline.com" do
+      env = {'HTTP_REFERER' => 'http://make-money-online.7makemoneyonline.com/money.php?u=http://monkey-robot.com'}
+      app.send(:pest?, env).should be true
+    end
+  end
+end
